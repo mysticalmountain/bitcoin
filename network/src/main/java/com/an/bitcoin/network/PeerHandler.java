5e94669a594b4354611e3fe44d1d5c33b1e9d67f
mysticalmountain <dongxudotan@163.com>
2019-05-20 10:43:57 +0800

    nothing

--- network/src/main/java/com/an/bitcoin/network/PeerHandler.java
+++ network/src/main/java/com/an/bitcoin/network/PeerHandler.java
@@ -2,14 +2,11 @@ package com.an.bitcoin.network;
 
 import com.an.bitcoin.network.handler.MessageHandlerManager;
 import com.an.bitcoin.protocol.Message;
-import com.an.bitcoin.protocol.Pong;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Random;
 
 /**
  * @ClassName PeerHandler
@@ -27,14 +24,7 @@ public class PeerHandler extends SimpleChannelInboundHandler<Message> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Message msg) throws Exception {
         logger.info("Receive message {}" + msg);
-
         MessageHandlerManager.getInstance().getMessageHandler(msg).handle(ctx, msg);
-
-//        Pong pong = new Pong(new Random().nextInt(Integer.MAX_VALUE));
-//        Message.Header header = new Message.Header(0x0709110B, "pong", Pong.LENGTH, 0);
-//        pong.setHeader(header);
-//        logger.info("Write message {}", pong);
-//        ctx.writeAndFlush(pong);
     }
 
     @Override
